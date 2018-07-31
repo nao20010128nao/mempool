374316decbe5ba2d1eb84bdf8880458e441de3b1
nao20010128nao <nao20010128@gmail.com>
2018-07-31 12:50:32 +0900

    HTTPS移行完了

--- web/queue/mempool.js
+++ web/queue/mempool.js
@@ -19,7 +19,7 @@
 var charts;
 var config = [
     {"name":"ZNY",
-     "url":"http://letsminezny.orz.hm/queue/",
+     "url":"https://letsminezny.orz.hm/queue/",
      "symbol":"ZNY",
      "satPerUnit": 100000000.0,
      "ranges": [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 14, 17, 20, 25, 30, 40, 50, 60, 70, 80, 100, 120, 140, 170, 200, 250, 300, 400, 500, 600, 700, 800, 1000, 1200, 1400, 1700, 2000, 2500, 3000, 4000, 5000, 6000, 7000, 8000, 10000 ],
